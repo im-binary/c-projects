c1b1dc939c462adcd25a725a4437480d5b993125
pongdang <pongdang0121@gmail.com>
2021-01-23 18:16:09 +0900

    3의 배수 5의 배수 구하기

---
+++ check-3-5/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// 숫자를 입력 받아서 3의 배수 또는 5의 배수면 t 아니면 f
+
+int main() {
+	int a;
+	scanf_s("%d", &a);
+
+	if ((a % 3 == 0))
+		printf("true\n");
+
+	else if ((a % 5 == 0))
+		printf("true\n");
+
+	else
+		printf("false\n");
+}
